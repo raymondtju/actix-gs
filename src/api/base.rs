3559c1f939132c3907516a0bdc5bf1e1560ad633
raymondtju <raymond.rtju@gmail.com>
2023-12-13 23:30:50 +0700

    feat: create and get products

--- src/api/base.rs
+++ src/api/base.rs
@@ -1,16 +1,21 @@
+use crate::AppState;
 use actix_web::{
   web::{self, get, resource},
   HttpResponse, Resource, Responder,
 };
+use chrono;
 use serde_json::json;
-
-use crate::AppState;
 
 async fn index(data: web::Data<AppState>) -> impl Responder {
   let app_name = &data.app_name;
   HttpResponse::Ok().json(json!({"name": format!("Hello {app_name}"),}))
 }
 
+async fn server_time() -> impl Responder {
+  HttpResponse::Ok().body(chrono::Utc::now().to_string())
+}
+
 pub fn services() -> Resource {
-  resource("/").route(get().to(index))
+  resource("/").route(get().to(index));
+  resource("/time").route(get().to(server_time))
 }
--- src/api/product.rs
+++ src/api/product.rs
@@ -1,21 +1,85 @@
 use actix_web::{
-  web::{get, resource, Query},
-  HttpResponse, Resource, Responder,
+  web::{get, post, resource, Json, Path},
+  HttpResponse, Resource,
 };
-use serde::Deserialize;
-use serde_json::json;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Serialize)]
+struct Product {
+  id: u32,
+  name: String,
+  price: u64,
+}
 
 #[derive(Deserialize)]
-struct ProductQuery {
-  product_id: Option<u32>,
+struct CreateProductDto {
+  name: String,
+  price: u64,
 }
 
-async fn get_one(query: Query<ProductQuery>) -> impl Responder {
-  HttpResponse::Ok().json(json!({
-    "query": query.product_id
-  }))
+fn product_data() -> Vec<Product> {
+  let data = vec![Product {
+    id: 1,
+    name: "Fashing Machine".to_string(),
+    price: 50000,
+  }];
+
+  return data;
 }
 
-pub fn services() -> Resource {
-  resource("/product").route(get().to(get_one))
+#[derive(Serialize)]
+struct ResponseSuccess {
+  status: u16,
+  message: String,
+  data: ResponseData,
+}
+
+#[derive(Serialize)]
+#[serde(untagged)]
+enum ResponseData {
+  Single(Product),
+  Multiple(Vec<Product>),
+}
+
+async fn get_all() -> HttpResponse {
+  HttpResponse::Ok().json(ResponseSuccess {
+    status: 200,
+    message: String::from("Get all products"),
+    data: ResponseData::Multiple(product_data()),
+  })
+}
+
+async fn get_one(path: Path<u32>) -> HttpResponse {
+  let id = path.into_inner();
+
+  let product_data = product_data();
+  let one_product: Option<&Product> = product_data.iter().find(|item: &&Product| item.id == id);
+
+  HttpResponse::Ok().json(ResponseSuccess {
+    status: 200,
+    message: format!("Get product with id {}", id),
+    data: ResponseData::Single(one_product.unwrap().clone()),
+  })
+}
+
+async fn create(body: Json<CreateProductDto>) -> HttpResponse {
+  HttpResponse::Ok().json(ResponseSuccess {
+    status: 200,
+    message: "Product created".to_string(),
+    data: ResponseData::Single(Product {
+      id: product_data().iter().last().unwrap().id + 1,
+      name: body.name.to_string(),
+      price: body.price,
+    }),
+  })
+}
+
+pub fn services() -> Vec<Resource> {
+  let mut resources = Vec::new();
+
+  resources.push(resource("/products").route(post().to(create)));
+  resources.push(resource("/products").route(get().to(get_all)));
+  resources.push(resource("/products/{id}").route(get().to(get_one)));
+
+  resources
 }
